604b8233f492bef25e3a5a2924b743808b2fa859
jjcruz99 <jjcruz139@gmail.com>
2025-02-02 18:48:29 -0500

    Agreagando carrusel proyectos y formulario contacto

--- script.js
+++ script.js
@@ -25,9 +25,107 @@ function reestablecerColor(){
 window.addEventListener('scroll', detectarMovimiento);
 
 
-
+///seccion competencias
 //competencias insertar imagenes
- document.getElementById('java').addEventListener('click', function(){
-     
- });
+const imgTecnologia = document.getElementById('img-tec');
+const conTextosTecnologia = document.getElementById('texto-competencia');
+const textosActuales = document.querySelectorAll('.texto-habilidad');
+//funcion para insertar imagen tecnologia
+function insertarimagen(direccion,textosActuales){
+  imgTecnologia.src = direccion;
 
+  conTextosTecnologia.innerHTML = textosActuales;
+}
+
+ document.getElementById('java').addEventListener('mousemove', function(){
+     const direccion = 'Pictures/java.png';
+     const textosDescripcion = `
+     <p class="texto-habilidad">-Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+     <p class="texto-habilidad">-Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+     <p class="texto-habilidad">-Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+         `;
+    insertarimagen(direccion,textosDescripcion);
+ });
+ document.getElementById('sprintboot').addEventListener('mousemove', function(){
+  const direccion = 'Pictures/boot_spring.png';
+  
+  const textosDescripcion = `
+    <p class="texto-habilidad">-Lo consectetur adipisicing elit. Hic, deserunt.</p>
+    <p class="texto-habilidad">-Lo consectetur adipisicing elit. Hic, deserunt.</p>
+    <p class="texto-habilidad">-Lo consectetur adipisicing elit. Hic, deserunt.</p>
+        `;
+   insertarimagen(direccion,textosDescripcion);
+});
+document.getElementById('github').addEventListener('mousemove', function(){
+  const direccion = 'Pictures/github.png';
+  const textosDescripcion = `
+  <p class="texto-habilidad">-Lorem ipsum dolor .</p>
+  <p class="texto-habilidad">-Lorem ipsum dolor .</p>
+  <p class="texto-habilidad">-Lorem ipsum dolor .</p>
+      `;
+ insertarimagen(direccion,textosDescripcion);
+});
+document.getElementById('sql').addEventListener('mousemove', function(){
+const direccion = 'Pictures/mysql.png';
+const textosDescripcion = `
+<p class="texto-habilidad">-Lorem  adipisicing elit. Hic, deserunt.</p>
+<p class="texto-habilidad">-Lorem ipsum dolor sit  adipisicing elit. Hic, deserunt.</p>
+<p class="texto-habilidad">-Lorem ipsum dolor sit adipisicing elit. Hic, deserunt.</p>
+    `;
+insertarimagen(direccion,textosDescripcion);
+});
+document.getElementById('js').addEventListener('mousemove', function(){
+  const direccion = 'Pictures/javaScript.png';
+  const textosDescripcion = `
+  <p class="texto-habilidad">-Lorem  dolor sit adipisicing elit. Hic, deserunt.</p>
+  <p class="texto-habilidad">-Lorem olor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+  <p class="texto-habilidad">-ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+      `;
+ insertarimagen(direccion,textosDescripcion);
+});
+document.getElementById('css').addEventListener('mousemove', function(){
+  const direccion = 'Pictures/css.png';
+  const textosDescripcion = `
+  <p class="texto-habilidad">- ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+  <p class="texto-habilidad">- ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+  <p class="texto-habilidad">- ipsum dolor sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+      `;
+ insertarimagen(direccion,textosDescripcion);
+});
+document.getElementById('html').addEventListener('mousemove', function(){
+const direccion = 'Pictures/html.png';
+const textosDescripcion = `
+<p class="texto-habilidad">-Lorem ipsum  sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+<p class="texto-habilidad">-Lorem ipsum sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+<p class="texto-habilidad">-Lorem ipsum  sit amet consectetur adipisicing elit. Hic, deserunt.</p>
+    `;
+insertarimagen(direccion,textosDescripcion);
+});
+
+
+////////seccion proyectos
+
+const carousel = document.querySelector('.carousel');
+const items = document.querySelectorAll('.carousel-item');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
+
+function updateCarousel() {
+    const itemWidth = items[0].clientWidth;
+    const newTransformValue = -currentIndex * itemWidth;
+    carousel.style.transform = `translateX(${newTransformValue}px)`;
+}
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : items.length - 1;
+    updateCarousel();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex < items.length - 1) ? currentIndex + 1 : 0;
+    updateCarousel();
+});
+
+// Inicializa el carrusel
+updateCarousel();
